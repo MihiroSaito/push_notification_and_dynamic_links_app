afd83bffb562c5de1a758e459312b43fb0d4f960
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2021-10-02 13:09:15 +0900

    プッシュ通知に画像をつけれるようにした

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by 齊藤実裕 on 2021/10/01.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
